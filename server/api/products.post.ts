1a0dc68e4a0c77a4b1eaebdcd20b997f46c3de71
Kylian, Jakub <jacob.kylian@gmail.com>
2023-06-29 01:11:13 +0200

    task/david-flek

--- server/api/products.post.ts
+++ server/api/products.post.ts
@@ -1,9 +1,40 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Product } from '@prisma/client';
 
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
-  // Implement your API here
+  if (!body.title || !body.price || !body.description) {
+    return {
+      statusCode: 400,
+      body: 'Missing name, price or description'
+    }
+  }
+  if (body.title.trim().length === 0 || body.price < 0 || body.description.trim().length === 0) {
+    return {
+      statusCode: 400,
+      body: 'Invalid name, price or description'
+    }
+  }
+
+
+  //přidat další validace
+
+  let product: Product;
+
+  try {
+    product = await prisma.product.create({
+      data: body
+    })
+
+  } catch (e) {
+
+    return {
+      statusCode: 500,
+      body: 'Error creating product, error: ' + e
+    }
+  }
+
+  return product
 })
